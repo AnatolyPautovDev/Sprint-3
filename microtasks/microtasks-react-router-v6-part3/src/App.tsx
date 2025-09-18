8b90ef6d48b795855d11295d020fc35db6e1413f
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-09-18 18:05:09 +0300

    react-router-practice

---
+++ microtasks/microtasks-react-router-v6-part3/src/App.tsx
@@ -0,0 +1,35 @@
+import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+
+export const App = ()=> {
+  return (
+    <div>
+      <div className={styles.header}><h1>HEADER</h1></div>
+      <div className={styles.body}>
+        <div className={styles.nav}>
+          <NavLink to={'/page1'} className={({isActive})=> isActive ? styles.activeNavLink : styles.navLink}>Page 1</NavLink>
+          <NavLink to={'/page2'} className={({isActive})=> isActive ? styles.activeNavLink : styles.navLink}>Page 2</NavLink>
+          <NavLink to={'/page3'} className={({isActive})=> isActive ? styles.activeNavLink : styles.navLink}>Page 3</NavLink>
+        </div>
+        <div className={styles.content}>
+          <Routes>
+            <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+
+            <Route path="/page1" element={<PageOne/>}  />
+            <Route path="/page2" element={<PageTwo/>}  />
+            <Route path="/page3" element={<PageThree/>}  />
+
+            <Route path={'/page/error'} element={<Error404 />} />
+            <Route path={'/*'} element={<Navigate to={'/page/error'} />}/>
+
+          </Routes>
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
+    </div>
+  );
+}
